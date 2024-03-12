76ebaa39962da689f883c06c3d6b2123e79cfc35
unknown <maksimkasol19@gmail.com>
2024-03-12 20:02:37 +0200

    Blabla

--- source.cpp
+++ source.cpp
@@ -5,14 +5,14 @@ int main() {
 	shape* array[5];
 	
 	rectangle rect(4, 5);
-	/*triangle tria(9,5,7);*/
+	triangle tria(9,5,7);
 	paralelo par(7,12);
-	/*paralelo trap(4, 13, 7, 8);*/
+	trapeze trap(4, 13, 7, 8);
 	
 	array[0] = &rect;
-	//array[1] = &tria;
+	array[1] = &tria;
 	array[2] = &par;
-	//array[3] = &trap;
+	array[3] = &trap;
 
 	for (int i = 0; i < 4; i++) {
 		cout << "Perimetr " << i + 1 << ": " << arr[i]->perimetr() << endl;
