fda4709c9677467ecf46c73e1530659fb72a301d
SpaceHunter19 <maksimkasol19@gmail.com>
2024-03-18 01:08:45 +0200

    Menu

--- source.cpp
+++ source.cpp
@@ -15,10 +15,25 @@ int main() {
 	array[3] = &trap;
 
 	
-	for (int i = 0; i < 4; i++) {
-		cout << "Perimeter " << i + 1 << ": " << array[i]->perimetr() << endl;
+	//for (int i = 0; i < 4; i++) {
+	//	cout << "Perimeter " << i + 1 << ": " << array[i]->perimetr() << endl;
+	//}
+	int menu;
+	cout << "What perimeter do you need?\n 1.Rectangle\n 2.Triangle\n 3.Paralelogram\n 4.Trapeze\n 5.All\nType number:"; cin >> menu;
+	switch (menu) {
+	case 1:
+		cout << "Rectangle perimeter = " << rect.perimetr() << endl; return main();
+	case 2:
+		cout << "Triangle perimeter = " << tria.perimetr() << endl; return main();
+	case 3:
+		cout << "Paralelogram perimeter = " << par.perimetr() << endl; return main();
+	case 4:
+		cout << "Trapeze perimeter = " << trap.perimetr() << endl; return main();
+	case 5:
+		for (int i = 0; i < 4; i++) {
+			cout << "Perimeter " << i + 1 << " = " << array[i]->perimetr() << endl;
+		}return main();
 	}
-
 
 	return 0;
 }
